e4a7a5332b9c506a682940d4e869ec58b8e0eda6
caio <chss.bolsa99@gmail.com>
2023-06-12 15:33:01 -0300

    tarefa 4 finalizada

--- pets/pet_serializers.py
+++ pets/pet_serializers.py
@@ -6,23 +6,17 @@ from traits.models import Trait
 from traits.trait_serializers import TraitSerializer
 from groups.group_serializers import GroupSerializer
 #from .models import Sex_choices
-from .models import models
-class Sex_choices(models.TextChoices):
-    MALE = "Male"
-    FEMALE = "Female"
-    NOT_INFORMED = "Not Informed"
+from .models import models, Sex_choices
+
 print(Sex_choices)
 class PetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=50)
     age = serializers.IntegerField()
     weight = serializers.FloatField()
-    sex = serializers.CharField(max_length=20, default=Sex_choices.NOT_INFORMED)
+    sex = serializers.ChoiceField(choices= Sex_choices.choices, default=Sex_choices.NOT_INFORMED)
     group = GroupSerializer()
     traits = TraitSerializer(many=True)
 
-    def validate_sex(self, value):
-        if value not in [choice[0] for choice in Sex_choices.choices]:
-            raise serializers.ValidationError("Invalid sex choice")
-        return value
+    
     
--- pets/views.py
+++ pets/views.py
@@ -70,29 +70,47 @@ class PetDetailView(APIView):
     def patch(self, request, pet_id):
        
         pet = get_object_or_404(Pet, id=pet_id)
+        serialized_pet = PetSerializer(pet,data=request.data,partial=True)
+        serialized_pet.is_valid(raise_exception=True)
+        group_data = serialized_pet.validated_data.pop("group",None)
+        traits_data = serialized_pet.validated_data.pop("traits",None)
 
-        group_data = request.data.get("group")
-        trait_data = request.data.get("traits")
-
+        print(group_data)
+        print(traits_data)
+        
         if group_data:
-            group_name = group_data["scientific_name"].lower()
-            group, _ = Group.objects.get_or_create(scientific_name=group_name)
+            group_name = group_data["scientific_name"]
+            group = Group.objects.filter(scientific_name__iexact=group_name).first()
+            if not group:
+                group = Group.objects.create(scientific_name=group_name)
             pet.group = group
-            pet.group.scientific_name = group_name
-            pet.group.save()
+            
+            print(pet.group)
+        
+        if traits_data:
+            #trait_names = [trait["name"] for trait in trait_data]
+            array = []
+            for trait in traits_data:
+                
+                objtraits = Trait.objects.filter(name__iexact=trait["name"]).first()
+                if not objtraits:
+                 objtraits = Trait.objects.create(**trait)
+            
+                pet.traits.add(objtraits)
+        
 
-        if trait_data:
-            trait_names = [trait["trait_name"].lower() for trait in trait_data]
-            traits = Trait.objects.filter(name__in=trait_names)
-            pet.traits.clear()
-            for trait in traits:
-                pet.traits.add(trait)
-
-        serializer = PetSerializer(instance=pet, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
+        pet.name = serialized_pet.validated_data.get("name", pet.name)
+        pet.age = serialized_pet.validated_data.get("age", pet.age)
+        pet.weight = serialized_pet.validated_data.get("weight", pet.weight)
+        pet.sex = serialized_pet.validated_data.get("sex", pet.sex)
         
         pet.save()
+        
+        serializer = PetSerializer(instance=pet)
+        
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
     def delete(self, request, pet_id):
             try:
                 pet = Pet.objects.get(id=pet_id)
